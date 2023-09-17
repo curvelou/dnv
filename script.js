cbdd944a342a609db8eb684b0343b4eb5b1250e3
curvelou <145216789+curvelou@users.noreply.github.com>
2023-09-17 02:03:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,99 +1,100 @@
-document.getElementById('sample-form').addEventListener('submit', function(e) {
-    e.preventDefault();
+async function fetchData(url, options = {}) {
+    try {
+        let response = await fetch(url, options);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        // Adicione aqui um feedback de erro na UI, se desejar
+    }
+}
 
-    let data = {
-        ponto: document.getElementById('ponto').value,
-        tratamento: document.getElementById('tratamento').value,
-        massa: document.getElementById('massa').value,
-        fragmentos: document.getElementById('fragmentos').value,
-        fibras: document.getElementById('fibras').value
+function getElementById(id) {
+    return document.getElementById(id);
+}
+
+function setupEventListeners() {
+    getElementById('sample-form').addEventListener('submit', async function(e) {
+        e.preventDefault();
+
+        let data = {
+            ponto: getElementById('ponto').value,
+            tratamento: getElementById('tratamento').value,
+            massa: getElementById('massa').value,
+            fragmentos: getElementById('fragmentos').value,
+            fibras: getElementById('fibras').value
+        };
+
+        let result = await fetchData('/add_sample', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        getElementById('result').innerText = result.message;
+    });
+
+    getElementById('get-samples-btn').addEventListener('click', async function() {
+        let data = await fetchData('/get_samples');
+        let resultDiv = getElementById('samples-result');
+        resultDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+    });
+
+    getElementById('export-samples-btn').addEventListener('click', function() {
+        window.location.href = '/export_samples';
+    });
+
+    getElementById('plot-samples-btn').addEventListener('click', async function() {
+        let data = await fetchData('/plot_samples');
+         var ids = data.map(d => d.id);
+    var massas = data.map(d => d.massa);
+    var fragmentos = data.map(d => d.fragmentos);
+    var fibras = data.map(d => d.fibras);
+
+    var trace1 = {
+        x: ids,
+        y: massas,
+        mode: 'markers',
+        type: 'scatter',
+        name: 'Massa',
+        marker: { color: 'blue' }
     };
 
-    fetch('/add_sample', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
+    var trace2 = {
+        x: ids,
+        y: fragmentos,
+        mode: 'markers',
+        type: 'scatter',
+        name: 'Fragmentos',
+        marker: { color: 'green' }
+    };
+
+    var trace3 = {
+        x: ids,
+        y: fibras,
+        mode: 'markers',
+        type: 'scatter',
+        name: 'Fibras',
+        marker: { color: 'red' }
+    };
+
+    var layout = {
+        title: 'Análise de Amostras',
+        xaxis: {
+            title: 'ID'
         },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('result').innerText = data.message;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
-// ... (restante do código JavaScript) ...
+        yaxis: {
+            title: 'Valor'
+        }
+    };
 
-document.getElementById('get-samples-btn').addEventListener('click', function() {
-    fetch('/get_samples')
-    .then(response => response.json())
-    .then(data => {
-        let resultDiv = document.getElementById('samples-result');
-        resultDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+    var config = {
+        responsive: true
+    };
+
+    Plotly.newPlot('samples-result', [trace1, trace2, trace3], layout, config);
 });
 
-document.getElementById('export-samples-btn').addEventListener('click', function() {
-    window.location.href = '/export_samples';
-});
-document.getElementById('plot-samples-btn').addEventListener('click', function() {
-    fetch('/plot_samples')
-    .then(response => response.json())
-    .then(data => {
-        var ids = data.map(d => d.id);
-        var massas = data.map(d => d.massa);
-        var fragmentos = data.map(d => d.fragmentos);
-        var fibras = data.map(d => d.fibras);
-
-        var trace1 = {
-            x: ids,
-            y: massas,
-            mode: 'markers',
-            type: 'scatter',
-            name: 'Massa',
-            marker: { color: 'blue' }
-        };
-
-        var trace2 = {
-            x: ids,
-            y: fragmentos,
-            mode: 'markers',
-            type: 'scatter',
-            name: 'Fragmentos',
-            marker: { color: 'green' }
-        };
-
-        var trace3 = {
-            x: ids,
-            y: fibras,
-            mode: 'markers',
-            type: 'scatter',
-            name: 'Fibras',
-            marker: { color: 'red' }
-        };
-
-        var layout = {
-            title: 'Análise de Amostras',
-            xaxis: {
-                title: 'ID'
-            },
-            yaxis: {
-                title: 'Valor'
-            }
-        };
-
-        var config = {
-            responsive: true
-        };
-
-        Plotly.newPlot('samples-result', [trace1, trace2, trace3], layout, config);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+document.addEventListener('DOMContentLoaded', setupEventListeners);
