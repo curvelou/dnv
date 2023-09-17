d6b2706e27897c896a96f05f2dcb1db221d299de
curvelou <145216789+curvelou@users.noreply.github.com>
2023-09-17 01:53:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,12 @@ def install_libraries():
 if __name__ == "__main__":
     install_libraries()
 
-
 from flask import Flask, render_template, request, jsonify, send_file
-from matplotlib import pyplot as plt
 import pandas as pd
 import sqlite3
-import os
 
 app = Flask(__name__)
+
 
 # Inicialização do banco de dados
 def init_db():
@@ -80,6 +78,13 @@ def plot_samples():
     plt.tight_layout()
     plot_file = '/path/to/directory/samples_plot.png'  # Substitua pelo caminho onde você deseja salvar o gráfico
     plt.savefig(plot_file)
+    app.route('/plot_samples', methods=['GET'])
+@def plot_samples():
+    conn = sqlite3.connect('samples.db')
+    df = pd.read_sql_query("SELECT * FROM samples", conn)
+    conn.close()
+
+    return df.to_json(orient='records')
     return send_file(plot_file, as_attachment=True)
 
 if __name__ == '__main__':
