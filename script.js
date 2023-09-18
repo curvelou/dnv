d128116ab2ea36e0bc9f0eb833622de71cf4062b
curvelou <145216789+curvelou@users.noreply.github.com>
2023-09-17 22:05:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,16 @@ function getElementById(id) {
     return document.getElementById(id);
 }
 
+function displayDataInTable(data) {
+    let table = '<table><tr><th>ID</th><th>Ponto</th><th>Tratamento</th><th>Massa</th><th>Fragmentos</th><th>Fibras</th></tr>';
+    data.forEach(row => {
+        table += `<tr><td>${row.id}</td><td>${row.ponto}</td><td>${row.tratamento}</td><td>${row.massa}</td><td>${row.fragmentos}</td><td>${row.fibras}</td></tr>`;
+    });
+    table += '</table>';
+    let resultDiv = getElementById('samples-result');
+    resultDiv.innerHTML = table;
+}
+
 function setupEventListeners() {
     getElementById('sample-form').addEventListener('submit', async function(e) {
         e.preventDefault();
@@ -37,64 +47,67 @@ function setupEventListeners() {
     });
 
     getElementById('get-samples-btn').addEventListener('click', async function() {
+        console.log("Botão 'Obter Amostras' clicado");
         let data = await fetchData('/get_samples');
-        let resultDiv = getElementById('samples-result');
-        resultDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
+        displayDataInTable(data);
     });
 
     getElementById('export-samples-btn').addEventListener('click', function() {
+        console.log("Botão 'Exportar Amostras' clicado");
         window.location.href = '/export_samples';
     });
 
     getElementById('plot-samples-btn').addEventListener('click', async function() {
+        console.log("Botão 'Plotar Amostras' clicado");
         let data = await fetchData('/plot_samples');
-         var ids = data.map(d => d.id);
-    var massas = data.map(d => d.massa);
-    var fragmentos = data.map(d => d.fragmentos);
-    var fibras = data.map(d => d.fibras);
+        var ids = data.map(d => d.id);
+        var massas = data.map(d => d.massa);
+        var fragmentos = data.map(d => d.fragmentos);
+        var fibras = data.map(d => d.fibras);
 
-    var trace1 = {
-        x: ids,
-        y: massas,
-        mode: 'markers',
-        type: 'scatter',
-        name: 'Massa',
-        marker: { color: 'blue' }
-    };
+        var trace1 = {
+            x: ids,
+            y: massas,
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Massa',
+            marker: { color: 'blue' }
+        };
 
-    var trace2 = {
-        x: ids,
-        y: fragmentos,
-        mode: 'markers',
-        type: 'scatter',
-        name: 'Fragmentos',
-        marker: { color: 'green' }
-    };
+        var trace2 = {
+            x: ids,
+            y: fragmentos,
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Fragmentos',
+            marker: { color: 'green' }
+        };
 
-    var trace3 = {
-        x: ids,
-        y: fibras,
-        mode: 'markers',
-        type: 'scatter',
-        name: 'Fibras',
-        marker: { color: 'red' }
-    };
+        var trace3 = {
+            x: ids,
+            y: fibras,
+            mode: 'markers',
+            type: 'scatter',
+            name: 'Fibras',
+            marker: { color: 'red' }
+        };
 
-    var layout = {
-        title: 'Análise de Amostras',
-        xaxis: {
-            title: 'ID'
-        },
-        yaxis: {
-            title: 'Valor'
-        }
-    };
+        var layout = {
+            title: 'Análise de Amostras',
+            xaxis: {
+                title: 'ID'
+            },
+            yaxis: {
+                title: 'Valor'
+            }
+        };
 
-    var config = {
-        responsive: true
-    };
+        var config = {
+            responsive: true
+        };
 
-    Plotly.newPlot('samples-result', [trace1, trace2, trace3], layout, config);
-});
+        Plotly.newPlot('samples-result', [trace1, trace2, trace3], layout, config);
+    });
+}
 
 document.addEventListener('DOMContentLoaded', setupEventListeners);
